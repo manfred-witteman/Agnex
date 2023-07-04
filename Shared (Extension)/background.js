32c749272430aaf9c8e65f75269411b15cf28141
Manfred <manfred@mac.com>
2023-07-04 16:41:56 +0200

    icons Added

--- Shared (Extension)/background.js
+++ Shared (Extension)/background.js
@@ -1,9 +1,16 @@
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var currentTab = tabs[0];
-    var currentUrl = currentTab.url;
-    var encodedUrl = encodeURIComponent(currentUrl);
-    var redirectUrl = "https://archive.is/submit/?url=" + encodedUrl;
-    chrome.tabs.update(currentTab.id, { url: redirectUrl });
-  });
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var currentTab = tabs[0];
+        var currentUrl = currentTab.url;
+        
+        // Check if the current URL is from the "archive.is" domain
+        if (currentUrl.includes("archive.is")) {
+            // Do not perform the redirect if the URL is from the "archive.is" domain
+            return;
+        }
+        
+        var encodedUrl = encodeURIComponent(currentUrl);
+        var redirectUrl = "https://archive.is/submit/?url=" + encodedUrl;
+        chrome.tabs.update(currentTab.id, { url: redirectUrl });
+    });
 });
